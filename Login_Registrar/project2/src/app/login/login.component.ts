ae325b41f92d69204a3671f6f19ccd53735f8774
Andy McBrayer <andrew.mcbrayer1@gmail.com>
2019-04-08 14:50:03 -0400

    restarting project

---
+++ Login_Registrar/project2/src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+export class LoginComponent implements OnInit{
+  loginForm: FormGroup;
+  loading = false;
+  sumitted=false;
+  returnUrl: string;
+  
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+}
+}
+
