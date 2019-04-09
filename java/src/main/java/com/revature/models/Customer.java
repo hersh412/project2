3dd5e784d2e28a656b4f863a10e8082f73a8721f
Josh <jdh428@gmail.com>
2019-04-09 09:51:19 -0400

    updated Customer pojo

--- java/src/main/java/com/revature/models/Customer.java
+++ java/src/main/java/com/revature/models/Customer.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +19,8 @@ public class Customer {
 	private int id;
 	private Date dob;
 	private String occupation;
+	@OneToOne
+	@JoinColumn(name="id")
 	private int state;
 	@Column(name="dl_issue_date")
 	private Date dlIssueDate;
