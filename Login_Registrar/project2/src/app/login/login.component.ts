4e5703c5172badb0f18064d2d9b87f18c7451cbc
Andy McBrayer <andrew.mcbrayer1@gmail.com>
2019-04-10 18:29:42 -0400

    Fixed all angular issues (now serves)

--- Login_Registrar/project2/src/app/login/login.component.ts
+++ Login_Registrar/project2/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+//import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 
@@ -10,10 +10,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl: string;
+ // loginForm: FormGroup;
+ // loading = false;
+ // submitted = false;
+ // returnUrl: string;
 
   constructor() {}
 
@@ -21,8 +21,8 @@ export class LoginComponent implements OnInit {
     // get return url from route parameters or default to '/'
   }
   onSubmit() {
-    this.submitted = true;
-    this.loading = true;
+  //  this.submitted = true;
+  //  this.loading = true;
   }
 }
 
