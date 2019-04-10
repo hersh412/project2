b40024ec17748702df595c9e79f8e77fe93a95cc
Andy McBrayer <andrew.mcbrayer1@gmail.com>
2019-04-10 12:51:03 -0400

    ill stop the world and melt with you
    There isn't anything that you and I wont do

--- Login_Registrar/project2/src/app/login/login.component.ts
+++ Login_Registrar/project2/src/app/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+//import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
-export class LoginComponent implements OnInit{
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   loading = false;
   submitted = false;
   returnUrl: string;
+
+  constructor() {}
 
   ngOnInit() {
     // get return url from route parameters or default to '/'
