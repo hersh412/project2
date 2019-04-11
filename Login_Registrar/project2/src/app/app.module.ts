0a16c35b4f8c8d1483cd20ff8e19e05dd411f300
Josh <jdh428@gmail.com>
2019-04-11 19:45:54 -0400

    updates

--- Login_Registrar/project2/src/app/app.module.ts
+++ Login_Registrar/project2/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NgxPopper } from 'angular-popper';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPopper
   ],
   providers: [],
   bootstrap: [AppComponent]
