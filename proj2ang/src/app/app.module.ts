9f7c1e0d1cfe2497b65acd48f2b6d89331377170
Josh <jdh428@gmail.com>
2019-04-12 20:52:28 -0400

    updates

--- proj2ang/src/app/app.module.ts
+++ proj2ang/src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    NgModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- proj2ang/src/app/navbar/navbar.component.ts
+++ proj2ang/src/app/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Users } from '../model/user.model';
-import { UsersService } from '../services/user.service';
+import { UsersService } from 'services/user.service';
 
 
 @Component({
