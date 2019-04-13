2af38f3fe20df51e20124ad3533d2022c07cd716
Josh <jdh428@gmail.com>
2019-04-13 16:18:20 -0400

    updated quote model

--- proj2java/src/main/java/com/revature/models/Quote.java
+++ proj2java/src/main/java/com/revature/models/Quote.java
@@ -4,22 +4,29 @@ import javax.persistence.Entity;
 
 @Entity
 public class Quote {
+	private long id;
 	private String zipcode;
-	private String make;
+	private String vehicleClass;
 	private String year;
 	private String gender;
 	private String age;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	public String getZipcode() {
 		return zipcode;
 	}
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
 	}
-	public String getMake() {
-		return make;
+	public String getVehicleClass() {
+		return vehicleClass;
 	}
-	public void setMake(String make) {
-		this.make = make;
+	public void setVehicleClass(String vehicleClass) {
+		this.vehicleClass = vehicleClass;
 	}
 	public String getYear() {
 		return year;
@@ -40,14 +47,65 @@ public class Quote {
 		this.age = age;
 	}
 	@Override
-	public String toString() {
-		return "Quote [zipcode=" + zipcode + ", make=" + make + ", year=" + year + ", gender=" + gender + ", age=" + age
-				+ "]";
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((vehicleClass == null) ? 0 : vehicleClass.hashCode());
+		result = prime * result + ((year == null) ? 0 : year.hashCode());
+		result = prime * result + ((zipcode == null) ? 0 : zipcode.hashCode());
+		return result;
 	}
-	public Quote(String zipcode, String make, String year, String gender, String age) {
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Quote other = (Quote) obj;
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (id != other.id)
+			return false;
+		if (vehicleClass == null) {
+			if (other.vehicleClass != null)
+				return false;
+		} else if (!vehicleClass.equals(other.vehicleClass))
+			return false;
+		if (year == null) {
+			if (other.year != null)
+				return false;
+		} else if (!year.equals(other.year))
+			return false;
+		if (zipcode == null) {
+			if (other.zipcode != null)
+				return false;
+		} else if (!zipcode.equals(other.zipcode))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Quote [id=" + id + ", zipcode=" + zipcode + ", vehicleClass=" + vehicleClass + ", year=" + year
+				+ ", gender=" + gender + ", age=" + age + "]";
+	}
+	public Quote(long id, String zipcode, String vehicleClass, String year, String gender, String age) {
 		super();
+		this.id = id;
 		this.zipcode = zipcode;
-		this.make = make;
+		this.vehicleClass = vehicleClass;
 		this.year = year;
 		this.gender = gender;
 		this.age = age;
