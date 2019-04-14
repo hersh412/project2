202034d843f1c3190112c76e416380696c9a941c
Josh <jdh428@gmail.com>
2019-04-13 22:48:39 -0400

    update Quote model to reflect timestamp
    Signed-off-by: Josh <jdh428@gmail.com>

--- proj2java/src/main/java/com/revature/models/Quote.java
+++ proj2java/src/main/java/com/revature/models/Quote.java
@@ -1,6 +1,8 @@
 package com.revature.models;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
 
 @Entity
 @Table(name = "quote")
@@ -8,6 +10,7 @@ public class Quote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private Timestamp timestamp;
 	private String zipcode;
     @Transient
     private String vehicleClass;
@@ -55,70 +58,55 @@ public class Quote {
 		this.age = age;
 	}
 
+    public Quote(long id, String zipcode, String vehicleClass, String year, String gender, String age) {
+        super();
+        this.id = id;
+        this.zipcode = zipcode;
+        this.vehicleClass = vehicleClass;
+        this.year = year;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
     @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((age == null) ? 0 : age.hashCode());
-		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
-		result = prime * result + (int) (id ^ (id >>> 32));
-		result = prime * result + ((vehicleClass == null) ? 0 : vehicleClass.hashCode());
-		result = prime * result + ((year == null) ? 0 : year.hashCode());
-		result = prime * result + ((zipcode == null) ? 0 : zipcode.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Quote other = (Quote) obj;
-		if (age == null) {
-			if (other.age != null)
-				return false;
-		} else if (!age.equals(other.age))
-			return false;
-		if (gender == null) {
-			if (other.gender != null)
-				return false;
-		} else if (!gender.equals(other.gender))
-			return false;
-		if (id != other.id)
-			return false;
-		if (vehicleClass == null) {
-			if (other.vehicleClass != null)
-				return false;
-		} else if (!vehicleClass.equals(other.vehicleClass))
-			return false;
-		if (year == null) {
-			if (other.year != null)
-				return false;
-		} else if (!year.equals(other.year))
-			return false;
-		if (zipcode == null) {
-			if (other.zipcode != null)
-				return false;
-		} else if (!zipcode.equals(other.zipcode))
-			return false;
-		return true;
-	}
-	@Override
-	public String toString() {
-		return "Quote [id=" + id + ", zipcode=" + zipcode + ", vehicleClass=" + vehicleClass + ", year=" + year
-				+ ", gender=" + gender + ", age=" + age + "]";
-	}
-	public Quote(long id, String zipcode, String vehicleClass, String year, String gender, String age) {
-		super();
-		this.id = id;
-		this.zipcode = zipcode;
-		this.vehicleClass = vehicleClass;
-		this.year = year;
-		this.gender = gender;
-		this.age = age;
-	}
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Quote quote = (Quote) o;
+        return id == quote.id &&
+                Objects.equals(timestamp, quote.timestamp) &&
+                Objects.equals(zipcode, quote.zipcode) &&
+                Objects.equals(vehicleClass, quote.vehicleClass) &&
+                Objects.equals(year, quote.year) &&
+                Objects.equals(gender, quote.gender) &&
+                Objects.equals(age, quote.age);
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "id=" + id +
+                ", timestamp=" + timestamp +
+                ", zipcode='" + zipcode + '\'' +
+                ", vehicleClass='" + vehicleClass + '\'' +
+                ", year='" + year + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, timestamp, zipcode, vehicleClass, year, gender, age);
+    }
 	public Quote() {
 		// TODO Auto-generated constructor stub
 	}
