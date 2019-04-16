a452175a0007590c1d94dd8ce9f3756dfd41f769
Josh <jdh428@gmail.com>
2019-04-16 18:43:54 -0400

    added quote component to routing

--- proj2ang/src/app/app.routing.ts
+++ proj2ang/src/app/app.routing.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { NavbarComponent } from '../app/navbar/navbar.component';
 
 import { AuthorizationGuard } from '../app/guards/authorization.guard';
+import { QuoteComponent } from './quote/quote.component';
 
 const appRoutes: Routes = [
     { path: '', component: NavbarComponent },
+    { path: '/quote', component: QuoteComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
