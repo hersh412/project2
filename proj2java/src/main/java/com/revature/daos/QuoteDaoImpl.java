14b7c150633999a76c2f5703b0517601ef49f246
Josh <jdh428@gmail.com>
2019-04-16 20:10:56 -0400

    added quotedao
    Signed-off-by: Josh <jdh428@gmail.com>

---
+++ proj2java/src/main/java/com/revature/daos/QuoteDaoImpl.java
@@ -0,0 +1,35 @@
+package com.revature.daos;
+
+import com.revature.models.Quote;
+import com.revature.utils.SessionFactoryUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuoteDaoImpl implements QuoteDao {
+
+    private SessionFactory sf = SessionFactoryUtil.getSessionFactory();
+    private Logger log = LogManager.getRootLogger();
+
+    public QuoteDaoImpl(SessionFactory sf) {
+        super();
+        this.sf = sf;
+    }
+
+    public QuoteDaoImpl() {
+    }
+
+    @Override
+    public void saveQuote(Quote quote) {
+        Session sess = sf.openSession();
+        Transaction t = sess.beginTransaction();
+        sess.persist(quote);
+        log.info("Adding quote " + quote.toString() + " to database.");
+        t.commit();
+        sess.close();
+    }
+}
