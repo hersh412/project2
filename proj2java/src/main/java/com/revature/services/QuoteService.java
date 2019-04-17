9944dd6ba6d019fb2fcbec93a941181782ee9854
Josh <jdh428@gmail.com>
2019-04-16 20:03:54 -0400

    updates for quote service

--- proj2java/src/main/java/com/revature/services/QuoteService.java
+++ proj2java/src/main/java/com/revature/services/QuoteService.java
@@ -1,9 +1,13 @@
 package com.revature.services;
 
+import java.util.List;
+
+import com.revature.models.Quote;
+
 public interface QuoteService {
-	
-	
-	
-	
+
+    void addQuote(Quote q);
+
+    List getAllQuotes();
 
 }
