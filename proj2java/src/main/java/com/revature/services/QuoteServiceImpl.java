69659bb09bfec991661db371b3bb067262e3de93
Josh <jdh428@gmail.com>
2019-04-16 20:09:16 -0400

    updates for quote service
    Signed-off-by: Josh <jdh428@gmail.com>

---
+++ proj2java/src/main/java/com/revature/services/QuoteServiceImpl.java
@@ -0,0 +1,39 @@
+package com.revature.services;
+
+import com.revature.daos.QuoteDao;
+import com.revature.models.Quote;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuoteServiceImpl implements QuoteService {
+
+    private static final Logger log = LogManager.getLogger(QuoteServiceImpl.class);
+    private QuoteDao qd;
+
+    public QuoteServiceImpl() {
+    }
+
+    public QuoteServiceImpl(QuoteDao qd) {
+        this.qd = qd;
+    }
+
+    @Override
+    public void addQuote(Quote q) {
+        qd.saveQuote(q);
+    }
+
+    @Override
+    public List getAllQuotes() {
+        return null;
+    }
+
+    @Autowired
+    public void setQd(QuoteDao qd) {
+        this.qd = qd;
+    }
+}
